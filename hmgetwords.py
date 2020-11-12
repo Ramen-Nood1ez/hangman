2fa9d9098ce0b6f37e298465238d914e2d6832a8
Ramen-Nood1ez <55363734+Ramen-Nood1ez@users.noreply.github.com>
2020-11-12 16:17:47 -0600

    added project

--- hmgetwords.py
+++ hmgetwords.py
@@ -7,7 +7,10 @@ def getwords():
     url = "https://Ramen-Nood1ez.github.io/hangman/index.html"
     uf = urllib.request.urlopen(url)
     html = uf.read()
-    var = re.sub("b*'", "", str(html))
-    var = re.split('"*"', string=var)
+    var = re.sub("b*'*\\r\\n+", "", str(html))
+    var = re.split('<p>*(...)*</p>+', string=var)
     print(var)
-    return var[1]
+    # return var[1]
+
+
+getwords()
