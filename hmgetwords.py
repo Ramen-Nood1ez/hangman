6dcfb11c639818cca9814dd5881a97b3ab3b3131
Ramen-Nood1ez <55363734+Ramen-Nood1ez@users.noreply.github.com>
2020-11-12 15:52:07 -0600

    changed index.html

--- hmgetwords.py
+++ hmgetwords.py
@@ -4,9 +4,10 @@ import re
 
 
 def getwords():
-    url = "https://Ramen-Nood1ez.github.io/hangman/hmmain.js"
+    url = "https://Ramen-Nood1ez.github.io/hangman/index.html"
     uf = urllib.request.urlopen(url)
     html = uf.read()
     var = re.sub("b*'", "", str(html))
     var = re.split('"*"', string=var)
+    print(var)
     return var[1]
